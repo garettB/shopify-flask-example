e36bff67e0b715cb31fd00e51b0631933b1ac35f
Paulina Khew <paulina.khew@shopify.com>
2021-02-16 14:16:31 -0500

    Add debug=True option for auto-reloading

--- src/server.py
+++ src/server.py
@@ -94,4 +94,4 @@ def data_removal_request():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=port)
